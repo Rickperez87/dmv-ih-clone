226532faba368b08a32efdd81fe1cf69bcf451b4
Rick Perez <Rickperez87@gmail.com>
2020-07-10 21:58:14 -0400

    added contact form pop up modal using JS

---
+++ app.js
@@ -0,0 +1,25 @@
+let contactForm = document.querySelector(".contactForm"),
+  modal = document.querySelector(".modal-container"),
+  closeBtn = document.querySelector(".close-btn");
+contactBtn = document.querySelector(".btn.btn-nav");
+
+// function displays contact form Modal
+let showContactForm = () => {
+  contactForm.style = "display:block";
+  modal.style = "display:block";
+};
+let closeContactForm = () => {
+  contactForm.style = "display:none";
+  modal.style = "display:none";
+};
+// event listner contact button
+contactBtn.addEventListener("click", () => {
+  showContactForm();
+});
+closeBtn.addEventListener("click", () => {
+  closeContactForm();
+});
+modal.addEventListener("click", () => {
+  closeContactForm();
+});
+// toggle modal class

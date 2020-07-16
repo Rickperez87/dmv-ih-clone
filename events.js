a1e634383b05d88b22187a7dbae72d39af69c726
Rick Perez <Rickperez87@gmail.com>
2020-07-16 13:08:37 -0400

    set submit event object to local storage

--- events.js
+++ events.js
@@ -172,16 +172,37 @@ function displayMonth(year, month) {
   }
 })();
 
-// add event to calander
-let eventDate = document.getElementById("eventDate"),
-  eventUrl = document.getElementById("eventUrl"),
-  submit = document
-    .getElementById("eventSubmit")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-      backgroundImage(eventUrl.value, eventDate.value);
-      console.log("works", eventUrl.value);
-    });
+class Store {
+  constructor(day, year, name, url) {
+    this.day = day;
+    this.year = year;
+    this.name = name;
+    this.url = url;
+  }
+}
+
+// On submit event date and URL stored in local storage
+function storeEvents(date, url) {
+  let month = date[0] + date[1] - 1,
+    day = date[3] + date[4],
+    year = date.slice(6),
+    name = `${Month.months[month]} ${day}`;
+  let event = new Store(day, year, name, url);
+  localStorage.setItem(name, JSON.stringify(event));
+  console.log(MonthsObj[month]);
+}
+
+// Submit url and date for future calendar event calls storeEvents
+let submit = document
+  .getElementById("eventSubmit")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    (eventDate = document.getElementById("eventDate")),
+      (eventUrl = document.getElementById("eventUrl"));
+    storeEvents(eventDate.value, eventUrl.value);
+  });
+
+//executes render of event=>  backgroundImage(eventUrl.value, eventDate.value);
 
 function backgroundImage(eventUrl, date) {
   let month, year, day, calendarELements;
@@ -192,11 +213,16 @@ function backgroundImage(eventUrl, date) {
     firstDay = d.getDay();
   calendarELements = firstDay + Number(day);
   console.log(calendarELements);
-
   let item = document.querySelector(
-    `tbody>tr:nth-child(${calendarELements / 7}) >td:nth-child(${
-      (calendarELements % 7) + 7
+    `tbody>tr:nth-child(${Math.ceil(calendarELements / 7)}) >td:nth-child(${
+      calendarELements % 7 || 7
     })`
   );
   item.style = `background:url(${eventUrl}) no-repeat center center/cover`;
 }
+
+//psuedo code.==>>
+//1. Store year object in local storage set events property false by default.
+//2.If submit event for the month set the object in storage events property to true.
+//2. have the change month event listner have a if event:true execute
+//function to retrieve from local storage and function to display on DOM.
